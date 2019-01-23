53148c4a61d0e02c9c017acbaa5b051547acd75d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-01-23 20:09:52 +0900

    loadVRM

---
+++ GotandaUnity10Example/Assets/LoadVRMSample.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using VRM;
+
+public class LoadVRMSample : MonoBehaviour
+{
+    [SerializeField] private string vrmurl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(downloadVRM(vrmurl));
+    }
+
+    private IEnumerator downloadVRM(string url){
+        UnityWebRequest webRequest = UnityWebRequest.Get(url);
+        yield return webRequest.SendWebRequest();
+        LoadVRM(webRequest.downloadHandler.data, (vrm) => {
+            
+        });
+    }
+
+    private void LoadVRM(byte[] vrmFileBinary, Action<GameObject> onLoaded){
+        VRMImporterContext context = new VRMImporterContext();
+        context.ParseGlb(vrmFileBinary);
+        context.LoadAsync((unit) =>
+        {
+            context.ShowMeshes();
+            onLoaded(context.Root);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
